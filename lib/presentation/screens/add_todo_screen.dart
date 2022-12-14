404d31f8117c89366e1c53d5e34f8c71e11fc4ef
HP Computer s <sohailokzz@gmail.com>
2022-12-14 14:41:19 +0500

    Initial structure app

---
+++ lib/presentation/screens/add_todo_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddTodoScreen extends StatelessWidget {
+  const AddTodoScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/presentation/screens/todo_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TodoScreen extends StatelessWidget {
+  const TodoScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Todo Screen"),
+      ),
+    );
+  }
+}
